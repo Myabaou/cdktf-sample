2e830e23771edbd727769ad21bfba64e18f98daf
Myabaou <ewdscxcx@gmail.com>
2025-07-13 17:09:11 +0900

    feat: add CloudFront CDN module and update README for module installation instructions

--- cdktf/lib/infra-stack.ts
+++ cdktf/lib/infra-stack.ts
@@ -1,9 +1,10 @@
 import { Construct } from "constructs";
-import { TerraformStack } from "cdktf";
+import { TerraformStack, TerraformOutput } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Environment } from "./environment";
 import { VpcModule } from "../modules/vpc";
 import { SqsModule } from "../modules/sqs";
+import { Cdn } from "../.gen/modules/cdn"; // Assuming you have a Cdn module
 
 interface InfraStackProps {
   stage: string;
@@ -11,9 +12,6 @@ interface InfraStackProps {
 }
 
 export class InfraStack extends TerraformStack {
-  public readonly vpcModule: VpcModule;
-  public readonly sqsModule: SqsModule;
-
   constructor(scope: Construct, id: string, props: InfraStackProps) {
     super(scope, id);
 
@@ -23,7 +21,7 @@ export class InfraStack extends TerraformStack {
     });
 
     // VPC Module
-    this.vpcModule = new VpcModule(this, "vpc", {
+    new VpcModule(this, "vpc", {
       stage: props.stage,
       cidr: props.environment.cidr,
       enableNatGateway: props.environment.enableNatGateway,
@@ -31,12 +29,56 @@ export class InfraStack extends TerraformStack {
     });
 
     // SQS Module
-    this.sqsModule = new SqsModule(this, "sqs", {
+    new SqsModule(this, "sqs", {
       stage: props.stage,
       enableDlq: props.environment.sqs.enableDlq,
       visibilityTimeoutSeconds: props.environment.sqs.visibilityTimeoutSeconds,
       messageRetentionSeconds: props.environment.sqs.messageRetentionSeconds,
       maxReceiveCount: props.environment.sqs.maxReceiveCount,
+    });
+
+    // Cloudfront CDN Module
+    const cloudFront = new Cdn(this, "cdn", {
+      createDistribution: true,
+      comment: `${props.stage} CloudFront Distribution`,
+      enabled: true,
+
+      // 必須: Origin設定（オブジェクト形式）
+      origin: {
+        "default-origin": {
+          domain_name: "1101.com", // 実際のオリジンドメインに変更してください
+          custom_origin_config: {
+            http_port: 80,
+            https_port: 443,
+            origin_protocol_policy: "https-only",
+            origin_ssl_protocols: ["TLSv1.2"],
+          },
+        },
+      },
+
+      // 必須: Default Cache Behavior（オブジェクト形式）
+      defaultCacheBehavior: {
+        target_origin_id: "default-origin",
+        viewer_protocol_policy: "redirect-to-https",
+        allowed_methods: [
+          "DELETE",
+          "GET",
+          "HEAD",
+          "OPTIONS",
+          "PATCH",
+          "POST",
+          "PUT",
+        ],
+        cached_methods: ["GET", "HEAD"],
+        compress: true,
+        query_string: false,
+        cookies_forward: "none",
+      },
+    });
+    // Cloudfrontの情報を出力
+    new TerraformOutput(this, "cloudfront_distribution_domain_name", {
+      value: cloudFront.cloudfrontDistributionDomainNameOutput,
+      description: "The domain name of the CloudFront distribution",
     });
   }
 }
